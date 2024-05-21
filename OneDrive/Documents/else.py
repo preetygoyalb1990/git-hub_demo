edcce35aa1e021b8aef1e91fa27d6fa7ed461470
alisha <rinkugoyalb@proton.me>
2024-05-21 11:59:49 +0100

    third commit

---
+++ OneDrive/Documents/else.py
@@ -0,0 +1,13 @@
+#x =0
+#while x <= 10: 
+    #print(x)
+    #x = x+1
+    #print("inside whileloop")
+    #print("out of while")
+
+
+    #city = [["india", "delhi"],["punjab" ,"moga"],[ "europe" , "itly"]]
+    #for c in city:
+        #print(c)
+
+    
