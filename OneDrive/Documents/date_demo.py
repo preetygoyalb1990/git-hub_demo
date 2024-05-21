480f24a68b0b7d51e6ea122bb729771a11ba5cb5
alisha <rinkugoyalb@proton.me>
2024-05-21 13:27:01 +0100

    this  is my commit

---
+++ OneDrive/Documents/date_demo.py
@@ -0,0 +1,4 @@
+import datetime
+
+current_date_demo = datetime.datetime.today().date()
+print(current_date_demo)
---
+++ OneDrive/Documents/grand.py
@@ -0,0 +1,16 @@
+#def var_scope_demo():
+    #x=20
+    #print(x)
+    #def var_scope_demo1():
+        #print(x)
+
+    #var_scope_demo1()
+#var_scope_demo()
+
+x= input("enter your name:")
+print("welcome" +x)
+
+        
+    
+    
+    
